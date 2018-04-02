0921b4b19c5b2a591bc7b588b597e4803ecd5089
FlorianDarfeuille <32645071+FlorianDarfeuille@users.noreply.github.com>
2018-04-02 19:57:23 +0200

    Nettoyage Arbo

--- application/views/ingredients/ingredients.php
+++
@@ -1,6 +0,0 @@
-<div class="control">
-  <input class="input" type="text" placeholder="Text input">
-</div>
-<div class="control">
-  <button class="button is-primary">Submit</button>
-</div>
--- db/setup_db.sh
+++
@@ -1,5 +0,0 @@
-# You need to create 'root' username and password before running the following
-
-echo "CREATE DATABASE IF NOT EXISTS Miam;" | sudo mysql -u root -proot
-sudo mysql -h localhost -u root Miam < init_db.sql
-echo "Script executed"
